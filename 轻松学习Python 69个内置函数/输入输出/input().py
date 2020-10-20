9cb62db5e183cda57f6a47a7f88437870bcffa14
周华健 <63383693+zhouhuajian-course@users.noreply.github.com>
2020-10-20 16:52:21 +0800

    Update input().py

--- 轻松学习Python 69个内置函数/输入输出/input().py
+++ 轻松学习Python 69个内置函数/输入输出/input().py
@@ -3,24 +3,24 @@
 
 # 如果存在 prompt 实参，则将其写入标准输出，末尾不带换行符。
 # 接下来，该函数从输入中读取一行，将其转换为字符串（除了末尾的换行符）并返回。
-# prompt = '请输入内容:'
-# line = input(prompt)
-# print(f'{ line = } { type(line) = }')
+prompt = '请输入内容:'
+line = input(prompt)
+print(f'{ line = } { type(line) = }')
 #
-# print(prompt, end='')
-# line = input()
-# print(f'{ line = } { type(line) = }')
+print(prompt, end='')
+line = input()
+print(f'{ line = } { type(line) = }')
 
 # 当读取到 EOF 时，则触发 EOFError。
-# while True:
-#     line = input()
-#     print(f'{ line = }')
-# while True:
-#     try:
-#         line = input()
-#         print(f'{ line = }')
-#     except EOFError:
-#         break
+while True:
+    line = input()
+    print(f'{ line = }')
+while True:
+    try:
+        line = input()
+        print(f'{ line = }')
+    except EOFError:
+        break
 
 
 
@@ -32,12 +32,12 @@
 # 如果加载了 readline 模块，input() 将使用它来提供复杂的行编辑和历史记录功能。
 
 # 如果加载了 readline 模块，input() 将使用它来提供复杂的行编辑和历史记录功能。
-# import readline
-# history_filepath = 'history.txt'
-# while True:
-#     line = input('请输入内容:')
-#     print(f'{ line = }')
-#     readline.write_history_file(history_filepath)
+import readline
+history_filepath = 'history.txt'
+while True:
+    line = input('请输入内容:')
+    print(f'{ line = }')
+    readline.write_history_file(history_filepath)
 
 #
 # 引发一个 审计事件 builtins.input 附带参数 prompt。
